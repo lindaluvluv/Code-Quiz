1115b6c7ecf4b15b1515c0eb6256dcd24f6e37c3
Linda Fan <f.linda97@gmail.com>
2021-09-29 07:54:51 -0700

    basic structure complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 //click start and it starts the timer
-var count = 60;
+var count = 100;
 var interval = setInterval();
 var Questions = document.getElementsByTagName("li");
 
@@ -9,61 +9,56 @@ setInterval(function(){
   if (count === 0){
     clearInterval(interval);
     document.getElementById('start').innerHTML='Done';
-//the quiz stops when the time runs out 
-    // return to the final page
   }
 }, 1000);
 
+document.getElementById('start').addEventListener("click", function(){
+  document.getElementById("begin").setAttribute("class", "hide");
+  document.getElementById("quiz").removeAttribute("class","hide");
+})
 
 const myQuestions = [
     {
       question: "What's the name of the little sister in Totoro?",
       answers: ["Mei","Setsuko","Satsuki"],
-      correctAnswer: "Setsuko"
+      correctAnswer: "Mei"
     },
-for (let index = 0; index < myQuestions[0].answers.length; index++) {
-    const element = array[index];
-    
-}
+
     {
-      question: "What is the best site ever created?",
-      answers: {
-        a: "SitePoint",
-        b: "Simple Steps Code",
-        c: "Trick question; they're both the best"
-      },
-      correctAnswer: "c"
+      question: "Which Studio Ghibli film was NOT directed by Hayao Miyazaki?",
+      answers: ["Kiki's Delivery Service","Grave of the Fireflies","Ponyo"],
+      correctAnswer: "Grave of the Fireflies"
     },
+
     {
-      question: "Where is Waldo really?",
-      answers: {
-        a: "Antarctica",
-        b: "Exploring the Pacific Ocean",
-        c: "Sitting in a tree",
-        d: "Minding his own business, so stop asking"
-      },
-      correctAnswer: "d"
-    }
+      question: "What's the name of the demon in Howl's Moving Castle?",
+      answers: ["Brinks","Calcifer","Bubbalo"],
+      correctAnswer: "Calcifer"
+    },
   ];
+for (let index = 0; index < myQuestions[0].answers.length; index++) {
+    const element = myQuestions[index].answers;
+console.log(myQuestions);
 
 //when click on the answer, jumps to the next question
-var clickAnswer = document.getElementsByClassName('options');
+// var clickAnswer = document.getElementsByClassName('options');
     
-    function navigate(direction) {
-        index = index + direction;
-        if (index < 0) { 
-          index = images.length - 1; 
-        } else if (index > Questions.length - 1) { 
-          index = 0;
-        }
-        else if (index === 4) {
-         //the quiz stops when all questions are answered
-            // jumps to the final page
-        }
-        currentQuestion = Questions[index];
+//     function navigate(direction) {
+//         index = index + direction;
+//         if (index < 0) { 
+//           index = images.length - 1; 
+//         } else if (index > Questions.length - 1) { 
+//           index = 0;
+//         }
+//         currentQuestion = Questions[index];
 
-clickAnswer.addEventListener ("click", navigate);
+// clickAnswer.addEventListener ("click", navigate);
 
+if (index === 2) {
+  function endGame() {
+    document.getElementById("quiz").setAttribute("class", "hide");
+    document.getElementById("endGame").removeAttribute("class","hide");
+};
 //when the answer is correct, write correcrt, otherwise wrong
 var correctAnswer=document.getElementsByClassName("correct");
 if (clickAnswer.val() === correctAnswer){
@@ -72,7 +67,15 @@ if (clickAnswer.val() === correctAnswer){
 } else {
     document.getElementById('correctOrNot').textContent("False!");
     count -= 10
-    count - 10 = count
+}
+
+var questionIndex = 1;
+
+myQuestions[questionIndex].question
+for (let i = 0; i < myQuestions[questionIndex].answers.length; i++) {
+    document.getElementsByClassName("options");
+    document.createElement("button");
+    document.getElementsByName("button").addEventListener("click", questionIndex++);
 }
 
 
